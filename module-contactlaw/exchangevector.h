fd265e639817c49a87ac7c020f9586e673440a87
tsukano <tsukano.fumitaka.699@gmail.com>
2024-12-03 21:41:49 +0900

    241203(軽微な修正)

--- module-contactlaw/exchangevector.h
+++ module-contactlaw/exchangevector.h
@@ -17,7 +17,7 @@ public:
     ////calculate lateral vector
     virtual Vec3 cross(Vec3& a, Vec3& b) const;
     virtual void unitVec_axial(Vec3& unitaxial, const Vec3& r1, const Vec3& r2) const;
-    virtual void unitVec_lateral(Vec3& unitlateral, const Vec3& r1, const Vec3& r2) const;
+    virtual void unitVec_lateral(Vec3& unitlateral, Vec3& v_lateral) const;
 };
 
 #endif // exchangevector_H
