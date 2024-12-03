5c5e31aca855b41d3c59d46db0b7db1a89f72ec5
tsukano <tsukano.fumitaka.699@gmail.com>
2024-12-03 21:44:52 +0900

    241203_v3(軽微な修正)

--- module-contactlaw/frictionforce.h
+++ module-contactlaw/frictionforce.h
@@ -11,8 +11,8 @@ public:
     virtual ~frictionforce(void);
 
     //calcurate friction force vector
-    virtual Vec3 f_friction_axial(doublereal& F, Vec3& v_axial)const;
-    virtual Vec3 f_friction_lateral(doublereal& F, Vec3& v_lateral)const;
+    virtual void f_friction_axial(doublereal& F, Vec3& v_axial)const;
+    virtual void f_friction_lateral(doublereal& F, Vec3& v_lateral)const;
 
 };
 #endif // FRICTIONAOFRCE_H
